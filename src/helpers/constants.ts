c7558f64ceadc4dd2bec9a52301271370cc350b0
hamza <hmmmzadev@gmail.com>
2025-07-21 16:22:59 +0500

    updated the bookingStatus to be more granular (according to the driver's app)

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,6 +4,8 @@ export enum BookingStatus {
   PENDING = "pending",
   AWAITING_ACCEPTANCE = "awaiting-acceptance",
   ASSIGNED = "assigned",
+  HEADING_TO_PICKUP = "heading-to-pickup",
+  ARRIVED_AT_PICKUP = "arrived-at-pickup",
   EN_ROUTE = "en-route",
   COMPLETED = "completed",
   CANCELLED = "cancelled"
